602459845fb3b741cb5dfdfb789436d0dd59125d
jinhobyeong <jhb_0921@naver.com>
2022-09-08 03:57:02 +0900

    chore : mainpage ui_2

--- src/page/MainPage.tsx
+++ src/page/MainPage.tsx
@@ -7,20 +7,25 @@ function MainPage() {
       className="flex justify-center font-press-start  h-screen"
       style={{ backgroundColor: ColorSystem.MainColor.Primary }}
     >
+      {/* 2023 */}
       <button className="flex justify-center h-fit  w-60 md:w-80 lg:w-2/6" type="button">
         <img src="images/newyearimg.png" alt="a" className="" />
       </button>
 
-      <div className="flex flex-col absolute bottom-0 left-0">
-        <button className=" static" type="button">
-          <img src="images/letterimg.png" alt="a" className="w-36" />
-        </button>
-
-        <button className="static" type="button">
-          <img src="images/thankimg.png" alt="a" className="w-36" />
+      <div className="absolute top-5 left-10">
+        <span className="flex justify-center text-white font-bold text-xl">ellie010707 님</span>
+      </div>
+      {/* 편지 */}
+      <div className="flex flex-col absolute top-5 left-5 md:m-10 w-20 md:w-28 lg:w-1/12 ">
+        <button type="button">
+          <img src="images/letterimg.png" alt="a" className="" />
         </button>
       </div>
-
+      {/* 추석 */}
+      <button className="absolute bottom-0 left-0 w-60 md:w-80 lg:w-2/6" type="button">
+        <img src="images/thankimg.png" alt="a" className=" " />
+      </button>
+      {/* 나무 */}
       <button className="" type="button">
         <img
           src="images/treeimg.png"
@@ -28,7 +33,7 @@ function MainPage() {
           className="absolute bottom-0 right-0 m-1 md:m-3  w-60 md:w-80 lg:w-2/6 "
         />
       </button>
-
+      {/* 호박 선물 */}
       <div className="flex flex-row absolute bottom-0 right-0 w-60 md:w-80 lg:w-2/5 ">
         <button className=" " type="button">
           <img src="images/halloweenimg.png" alt="a" className="" />
