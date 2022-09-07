0ebd3c114e585a80de486cc05ca8cd5a02d8c00d
yura <rladbfk0000@naver.com>
2022-09-07 17:14:08 +0900

    feat:test

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,8 @@ import 'tailwindcss/tailwind.css';
 
 function App() {
   return (
-    <div className="flex justify-center">
-      <span className="text-xl text-blue-700">letter-man Project Start !!!!</span>
-    </div>
+    <div>dds</div>
+      
   );
 }
 
