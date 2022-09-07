86e5a6a71dfb6a8f6eef0e591ce8b0acf338bd79
jinhobyeong <jhb_0921@naver.com>
2022-09-07 17:05:42 +0900

    chore : add folder

---
+++ src/components/index.tsx
@@ -0,0 +1,11 @@
+import 'tailwindcss/tailwind.css';
+
+function test() {
+  return (
+    <div className="flex justify-center">
+      <span className="text-xl text-blue-700">index</span>
+    </div>
+  );
+}
+
+export default test;
