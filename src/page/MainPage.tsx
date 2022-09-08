d6c52f337fb50c5c330ec6d83caf58136db2d392
jinhobyeong <jhb_0921@naver.com>
2022-09-08 13:11:37 +0900

    chore : mainpage ui_animation

--- src/page/MainPage.tsx
+++ src/page/MainPage.tsx
@@ -1,14 +1,20 @@
 import 'tailwindcss/tailwind.css';
 import ColorSystem from 'utils/ColorSystem';
+import React from 'react';
+import './pageStyle.css';
 
 function MainPage() {
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    console.log(event.target);
+  };
+
   return (
     <div
       className="flex justify-center font-press-start  h-screen"
       style={{ backgroundColor: ColorSystem.MainColor.Primary }}
     >
       {/* 2023 */}
-      <button className="flex justify-center h-fit  w-60 md:w-80 lg:w-2/6" type="button">
+      <button onClick={handleClick} className="scaleup flex justify-center h-fit  w-60 md:w-80 lg:w-2/6" type="button">
         <img src="images/newyearimg.png" alt="a" className="" />
       </button>
 
@@ -17,29 +23,29 @@ function MainPage() {
       </div>
       {/* 편지 */}
       <div className="flex flex-col absolute top-5 left-5 md:m-10 w-20 md:w-28 lg:w-1/12 ">
-        <button type="button">
-          <img src="images/letterimg.png" alt="a" className="" />
+        <button onClick={handleClick} type="button" className="scaleup">
+          <img src="images/letterimg.png" alt="a" />
         </button>
       </div>
       {/* 추석 */}
-      <button className="absolute bottom-0 left-0 w-60 md:w-80 lg:w-2/6" type="button">
-        <img src="images/thankimg.png" alt="a" className=" " />
+      <button onClick={handleClick} className="scaleup absolute bottom-0 left-0 w-60 md:w-80 lg:w-2/6" type="button">
+        <img src="images/thankimg.png" alt="a" />
       </button>
       {/* 나무 */}
-      <button className="" type="button">
+      <button onClick={handleClick} className="" type="button">
         <img
           src="images/treeimg.png"
           alt="a"
-          className="absolute bottom-0 right-0 m-1 md:m-3  w-60 md:w-80 lg:w-2/6 "
+          className="scaleup absolute bottom-0 right-0 m-1 md:m-3  w-60 md:w-80 lg:w-2/6 "
         />
       </button>
       {/* 호박 선물 */}
       <div className="flex flex-row absolute bottom-0 right-0 w-60 md:w-80 lg:w-2/5 ">
-        <button className=" " type="button">
-          <img src="images/halloweenimg.png" alt="a" className="" />
+        <button onClick={handleClick} className="scaleup" type="button">
+          <img src="images/halloweenimg.png" alt="a" className="origin-center hover:origin-top" />
         </button>
-        <button className="" type="button">
-          <img src="images/valentineimg.png" alt="a" className="" />
+        <button onClick={handleClick} className="scaleup" type="button">
+          <img src="images/valentineimg.png" alt="a" />
         </button>
       </div>
     </div>
