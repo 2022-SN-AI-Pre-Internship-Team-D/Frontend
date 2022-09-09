c1f73f3d06da649e305a85817e88e0aa3f56ef1f
jinhobyeong <jhb_0921@naver.com>
2022-09-09 17:28:03 +0900

    chore : mailresult ui_1(modal)

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LoginPage from 'page/LoginPage';
 import MainPage from 'page/MainPage';
 import RemainingDaysPage from 'page/RemainingDaysPage';
+import ResultModal from 'components/ResultModal';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<LoginPage/>} />
         <Route path="/" element={<MainPage />} />
         <Route path="/remainingdayspage" element={<RemainingDaysPage />} />
+        <Route path="/test" element={<ResultModal />} />
       </Routes>
     </Router>
   );
