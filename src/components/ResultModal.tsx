eb83dc7f2915b46a0164a30514a17f6314bf08ae
jinhobyeong <jhb_0921@naver.com>
2022-09-09 17:53:30 +0900

    chore : mailresult ui_2

--- src/components/ResultModal.tsx
+++ src/components/ResultModal.tsx
@@ -1,5 +1,6 @@
 import { Fragment, useRef, useState } from 'react';
 import { Dialog, Transition } from '@headlessui/react';
+import ColorSystem from 'utils/ColorSystem';
 
 export default function ResultModal() {
   const [open, setOpen] = useState(false);
@@ -42,30 +43,29 @@ export default function ResultModal() {
                 leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
               >
                 <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
-                  <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                  <div
+                    className="flex justify-center bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4"
+                    style={{ backgroundColor: ColorSystem.MainColor.Primary }}
+                  >
                     <div className="sm:flex sm:items-start">
                       <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
                         <Dialog.Title as="h3" className="text-lg font-medium leading-6 text-gray-900">
                           Deactivate account
                         </Dialog.Title>
-                        <div className="mt-2">
-                          <p className="text-sm text-gray-500">
-                            Are you sure you want to deactivate your account? All of your data will be permanently
-                            removed. This action cannot be undone.
-                          </p>
+                        <div className="mt-2 flex justify-center">
+                          <img src="images/letterimg.png" alt="a" className="w-48" />
                         </div>
                       </div>
                     </div>
                   </div>
-                  <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
-                    <button
-                      type="button"
-                      className="inline-flex w-full justify-center rounded-md border border-transparent bg-red-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm"
-                      onClick={() => setOpen(false)}
-                    >
-                      Deactivate
-                    </button>
-                  </div>
+
+                  <button
+                    type="button"
+                    className="absolute top-0 right-0 inline-flex w-full justify-center rounded-full border border-transparent bg-slate-700 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm"
+                    onClick={() => setOpen(false)}
+                  >
+                    X
+                  </button>
                 </Dialog.Panel>
               </Transition.Child>
             </div>
