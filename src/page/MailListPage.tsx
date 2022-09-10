5617d02784896e30e62802f1c0260ea51c47235c
jinhobyeong <jhb_0921@naver.com>
2022-09-10 22:20:19 +0900

    chore : mailresult ui finish

--- src/page/MailListPage.tsx
+++ src/page/MailListPage.tsx
@@ -8,6 +8,7 @@ function MailListPage() {
       className="flex justify-center items-center flex-col"
       style={{ backgroundColor: ColorSystem.MainColor.Primary }}
     >
+      <span className="text-white m-14 text-2xl">총 n개의 편지를 받았습니다.</span>
       <EachMail />
       <EachMail />
       <EachMail />
