10430cc05340b63977f86fea316feeb1f0b2118e
jinhobyeong <jhb_0921@naver.com>
2022-09-11 23:13:09 +0900

    fix : app.tsx error

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,21 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import LoginPage from 'page/LoginPage';
 import MailListPage from 'page/MailListPage';
+import MailWritePage from 'page/MailWritePage';
 import SigninPage from 'page/SigninPage';
 import MainPage from './page/MainPage';
-import MailWritePage from 'page/MailWritePage';
 import RemainingDaysPage from './page/RemainingDaysPage';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<LoginPage/>} />
+        <Route path="/" element={<LoginPage />} />
         <Route path="/" element={<MainPage />} />
         <Route path="/remainingdayspage" element={<RemainingDaysPage />} />
         <Route path="/maillistpage" element={<MailListPage />} />
         <Route path="/mailwritepage" element={<MailWritePage />} />
-        <Route path="/signin" element={<SigninPage/>} />
+        <Route path="/signin" element={<SigninPage />} />
       </Routes>
     </Router>
   );
