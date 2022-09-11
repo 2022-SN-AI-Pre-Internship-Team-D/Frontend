177541479833a78fe2417f1e0fcfefc21dc75480
yura <rladbfk0000@naver.com>
2022-09-11 23:32:56 +0900

    chore : mypage ui_1

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LoginPage from 'page/LoginPage';
 import MainPage from 'page/MainPage';
 import RemainingDaysPage from 'page/RemainingDaysPage';
+import MyPage from  'page/Mypage';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<LoginPage/>} />
         <Route path="/" element={<MainPage />} />
         <Route path="/remainingdayspage" element={<RemainingDaysPage />} />
+        <Route path="/mypage" element={<MyPage/>} />
       </Routes>
     </Router>
   );
---
+++ src/page/Mypage.tsx
@@ -0,0 +1,38 @@
+import 'tailwindcss/tailwind.css';
+import  userprofile from '../images/userprofile.png'
+
+function MyPage() {
+    return (
+        
+    <div 
+    className=' pt-20 bg-[#0E1733] flex justify-center h-[60rem] items-center'>
+        
+        <div className=' text-white border-solid border-2 rounded-lg w-4/6 h-4/5 flex flex-col items-center justify-center'>
+
+        <img style={{position:'absolute',top:'10px'}} className='scale-75 object-fit:cover text-white border-2 rounded-full' src={userprofile} alt="유저사진" />
+
+        <div className='mb-10 pt-14 text-2xl font-mypage-font flex justify-center'>EllieKim010707</div>
+
+        <div className='flex flex-col justify-center items-center'>
+            <h2 className='font-press-start mb-5 text-3xl'>Your Anniversary</h2>
+            <ul className='list-disc'>
+                <li>Birthday [2001 - 07 - 07]</li>
+                <li>Birthday [2001 - 03 - 02]</li>
+            </ul>
+        </div>
+
+        <div className='flex flex-col justify-center items-center'>
+            <h2 className='font-press-start mt-20 mb-5 text-3xl'>Letters</h2>
+            <ul className='list-disc flex flex-col justify-center items-center'>
+                <li>Birthday [70]</li>
+                <li className='mb-10'>Christmas [327]</li>
+            </ul>
+        </div>
+
+        <button type='button' className='font-press-start mt-10'>Account Settings</button>
+    </div>
+    </div>
+    )
+}
+
+export default MyPage;
