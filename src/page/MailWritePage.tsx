b56864bcdfdeecaad97e2d8b593d3d188d9ac364
jinhobyeong <jhb_0921@naver.com>
2022-09-11 23:10:55 +0900

    chore : add craco finish

--- src/page/MailWritePage.tsx
+++ src/page/MailWritePage.tsx
@@ -1,7 +1,6 @@
 import 'tailwindcss/tailwind.css';
 import React, { useState } from 'react';
 import ColorSystem from 'utils/ColorSystem';
-import MailInput from 'components/MailList/MailInput';
 import postcard from '@images/postcard.png';
 
 function MailWritePage() {
