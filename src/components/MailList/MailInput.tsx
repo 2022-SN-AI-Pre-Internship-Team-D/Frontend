618849c1252ab39f7cb1796cdd7460fe8ab9261f
jinhobyeong <jhb_0921@naver.com>
2022-09-17 02:35:26 +0900

    feat : pagenation(birth) api_1

--- src/components/MailList/MailInput.tsx
+++ src/components/MailList/MailInput.tsx
@@ -1,7 +1,7 @@
 import 'tailwindcss/tailwind.css';
 import { useState } from 'react';
 
-function MailInput() {
+function MailInput({ content = '' }) {
   const [test, setTest] = useState('');
   console.log(test);
 
@@ -13,6 +13,7 @@ function MailInput() {
         cols={45}
         rows={9}
         maxLength={300}
+        value={content}
         className="p-4 rounded-lg bg-transparent text-xl leading-9 focus:outline-none "
         onChange={(e) => console.log(e.target.value)}
       />
