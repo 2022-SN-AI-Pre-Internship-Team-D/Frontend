56c8782d1bb3f658255cd506ffa677f55f128439
suhyeon <suhyeon3484@naver.com>
2022-09-20 02:20:06 +0900

    chore : 기본 이벤트 편지 개수의 api 연결

--- src/page/Mypage.tsx
+++ src/page/Mypage.tsx
@@ -1,7 +1,23 @@
 import 'tailwindcss/tailwind.css';
 import userprofile from 'images/userprofile.png';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function MyPage() {
+  const [mailCount, setMailCount] = useState('0');
+  useEffect(() => {
+    (async () => {
+      await axios
+        .get(`letters/users/64a00ada-ffa3-4607-b282-dee397e93d57/events/75c5abfd-7b8f-448e-ba9c-caf780218f04/counts`)
+        .then((res) => {
+          setMailCount(res.data[0].count);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    })();
+  }, []);
+
   return (
     <div className=" pt-20 bg-[#0E1733] flex justify-center h-screen items-center">
       <div className=" text-white border-solid border-2 rounded-lg w-4/6 h-4/5 flex flex-col items-center justify-center">
@@ -25,8 +41,8 @@ function MyPage() {
         <div className="flex flex-col justify-center items-center">
           <h2 className="font-press-start mt-20 mb-5 text-3xl">Letters</h2>
           <ul className="list-disc flex flex-col justify-center items-center">
-            <li>Birthday [70]</li>
-            <li className="mb-10">Christmas [327]</li>
+            <li>Birthday [{mailCount}]</li>
+            <li className="mb-10">Christmas [{mailCount}]</li>
           </ul>
         </div>
 
