de2197748acd1422215185b1b130d4fd0460def8
suhyeon <suhyeon3484@naver.com>
2022-09-30 01:28:07 +0900

    chore : add event

--- src/page/Mypage.tsx
+++ src/page/Mypage.tsx
@@ -17,6 +17,8 @@ function MyPage() {
   const [newYearMail, setNewYearMail] = useState('0');
   const [halloweenMail, sethalloweenMail] = useState('0');
   const [christmasMail, setChristmasMail] = useState('0');
+  const [chuseok, setChuseok] = useState('0');
+  const [children, setChildren] = useState('0');
 
   const arrEvent: any = [];
 
@@ -87,6 +89,30 @@ function MyPage() {
             console.log(error);
           });
       })();
+
+      // 추석
+      (async () => {
+        await axios
+          .get(`letters/users/${uuid}/events/${arrEvent[3]}/counts`)
+          .then((res) => {
+            setChuseok(res.data[0].count);
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      })();
+
+      // 어린이날
+      (async () => {
+        await axios
+          .get(`letters/users/${uuid}/events/${arrEvent[4]}/counts`)
+          .then((res) => {
+            setChildren(res.data[0].count);
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      })();
     });
   }, []);
 
@@ -124,6 +150,8 @@ function MyPage() {
             <li>New Year [{newYearMail}]</li>
             <li>Halloween [{halloweenMail}]</li>
             <li>Christmas [{christmasMail}]</li>
+            <li>Chuseok [{chuseok}]</li>
+            <li>Children&apos;s Day [{children}]</li>
           </ul>
         </div>
         {/* <button type="button" className="font-press-start mt-10">
